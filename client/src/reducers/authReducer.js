fb15579888c763cabdad74385996e55b5cd126e8
Domitrius Clark <domitriusaclark@gmail.com>
2018-03-29 22:53:22 -0400

    Added first two components and materialize for styling.

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,7 @@
+// Using camelcase on the filename indicates exporting a function 
+export default function (state = {}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
